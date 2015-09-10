ec36ab097988f1515f1a97397dc54ed97df10767
Yang Fan <whitedeath.yf@gmail.com>
2015-09-10 11:49:02 +0800

    Add local Notification Practice

--- remember/AppDelegate.swift
+++ remember/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let notificationSettings = UIUserNotificationSettings(
+            forTypes: .Alert | .Badge | .Sound, categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings( notificationSettings)
         return true
     }
 
@@ -39,6 +42,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        println("didReceiveLocalNotification \(notification)")
+        application.applicationIconBadgeNumber = 0
     }
 
 
--- remember/ViewController.swift
+++ remember/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let date = NSDate(timeIntervalSinceNow: 5)
+        let localNotification = UILocalNotification()
+        localNotification.fireDate = date
+        localNotification.timeZone = NSTimeZone.defaultTimeZone()
+        localNotification.alertBody = "I am a local notification!"
+        localNotification.hasAction = true
+        localNotification.alertAction = "可以继续调戏"
+        localNotification.soundName = UILocalNotificationDefaultSoundName
+        localNotification.applicationIconBadgeNumber = 1990
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
     }
 
     override func didReceiveMemoryWarning() {
